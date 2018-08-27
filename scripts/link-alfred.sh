f2270741f0e106ebe3943255696f34ae1d6793bd
Jarle Fosen <jarlefosen@gmail.com>
2018-08-27 19:00:28 +0200

    Only force linkin if destination do not exist or is a symlink

--- scripts/link-alfred.sh
+++ scripts/link-alfred.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 _thisdir=$(realpath "$(dirname "$0")")
 _repodir=$(realpath "${_thisdir}/../")
+_target="$HOME/Library/Application Support/Alfred 3/Alfred.alfredpreferences/workflows/$(basename ${_repodir})"
 
-echo "Linking \"$(basename "${_repodir}")\" to Alfred 3 workflows"
+echo "Linking \"$(basename "${_repodir}")\" to Alfred 3 workflows to ${_target}"
 
-if ! ln -fs "$_repodir" "$HOME/Library/Application Support/Alfred 3/Alfred.alfredpreferences/workflows/."
+# Check if target is an actual file
+if [ -f "$_target" ] || [ -d "$_target" ] && ! [ -L "$_target" ]
+then
+    echo "ERROR: Target exists and is an actual file or directory" >>/dev/stderr
+    exit 1
+fi
+
+if ! ln -hfs "$_repodir" "$_target"
 then
     echo ""                                            >> /dev/stderr
     echo "ERROR or WARNING or SOMETHING"               >> /dev/stderr
     echo "  Alfred 3 is not installed on this machine" >> /dev/stderr
     echo "  or"                                        >> /dev/stderr
     echo "  this Alfred script is already installed"   >> /dev/stderr
+    exit 1
 fi
